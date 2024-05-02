46ada32798c37425b97ac895fb80caab0f1e0060
glafratta <g.lafratta@outlook.com~>
2024-05-02 16:14:19 +0100

    print

--- qtviewer/window.h
+++ qtviewer/window.h
@@ -27,12 +27,12 @@ public:
 
     struct MyCallback : Libcam2OpenCV::Callback {
 	Window* window = nullptr;
-    int frame=0;
+    int it=0;
 	virtual void hasFrame(const cv::Mat &frame, const libcamera::ControlList &) {
-        frame++;
+        it++;
         cv::Mat frame_grey;
         cv::cvtColor(frame, frame_grey, cv::COLOR_RGB2GRAY);
-        if (frame==1){
+        if (it==1){
             printf("frame col %i and rows %i, channels %i, size:%i\n", frame.cols, frame.rows, frame.channels(), sizeof(frame[0][0]));
             printf("frame_grey col %i and rows %i, channels %i, size:%i\n", frame_grey.cols, frame_grey.rows, frame_grey.channels(), sizeof(frame_grey[0][0]));
 
